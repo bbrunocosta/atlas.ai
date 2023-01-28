7d196161860c0d7f345e619c57218799e24a7105
Bruno Costa Borges <brunocosta@wolfplace.solutions>
2023-01-28 15:11:34 -0300

    feat: add code that gets Dall-e response

--- open-ai.js
+++ open-ai.js
@@ -29,3 +29,19 @@ export const getDavinciResponse = async (clientText) => {
         return `❌ OpenAI Response Error: ${e.response.data.error.message}`;
     }
 }
+
+
+export const getDalleResponse = async (clientText) => {
+    const options = {
+        prompt: clientText,
+        n: 1,
+        size: "1024x1024",
+    };
+
+    try {
+        const response = await openai.createImage(options);
+        return response.data.data[0].url;
+    } catch (e) {
+        return `❌ OpenAI Response Error: ${e.response.data.error.message}`;
+    }
+}
