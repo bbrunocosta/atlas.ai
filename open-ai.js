029e0b61d73a22cccd78fe7b6560eb89e56f1143
Bruno Costa Borges <brunocosta@wolfplace.solutions>
2023-01-28 15:10:56 -0300

    feat: add code that gets davinci response

--- open-ai.js
+++ open-ai.js
@@ -7,3 +7,25 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+
+
+export const getDavinciResponse = async (clientText) => {
+    const options = {
+        model: "text-davinci-003",
+        prompt: clientText,
+        temperature: 1,
+        max_tokens: 4000
+    };
+
+    try {
+        const response = await openai.createCompletion(options);
+        let botResponse = "";
+        response.data.choices.forEach(({ text }) => {
+            botResponse += text
+        });
+        return `Chat GPT 🤖\n\n ${botResponse.trim()}`;
+    } catch (e) {
+        console.log(e);
+        return `❌ OpenAI Response Error: ${e.response.data.error.message}`;
+    }
+}
