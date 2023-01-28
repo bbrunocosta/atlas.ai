99278a2569da72f1036bf4758e90607c2dbd0c9d
Bruno Costa Borges <brunocosta@wolfplace.solutions>
2023-01-28 17:22:14 -0300

    feat: add WhatsApp ResponseTypes and Handlers

---
+++ src/whatsapp/responseHandlers.js
@@ -0,0 +1,21 @@
+import * as responseTypes from "./responseTypes.js";
+
+
+export const sendResponseAsText = (client, message, text ) => client.sendText(
+    message.from === process.env.WHATSAPP_PHONE_NUMBER ? message.to : message.from,
+    text
+)
+
+export const sendResponseAsImg= (client, message, imgUrl, imgDescription ) => client.sendImage(
+    message.from === process.env.WHATSAPP_PHONE_NUMBER ? message.to : message.from,
+    imgUrl,
+    imgDescription,
+    'Imagem gerada pela IA DALL-E 🤖'
+);
+
+const responseHandlerCollection = {
+    [responseTypes.text]: sendResponseAsText,
+    [responseTypes.img]: sendResponseAsImg
+}
+
+export const getResponseHandler = responseType => (responseHandlerCollection[responseType] || null)
---
+++ src/whatsapp/responseTypes.js
@@ -0,0 +1,3 @@
+
+export const text = 'text'
+export const img =  'img'
