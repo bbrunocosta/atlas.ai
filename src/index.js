03e0403f786fb0f7c06697994c9ea151a904cb37
Bruno Costa Borges <brunocosta@wolfplace.solutions>
2025-02-04 00:24:52 -0300

    fix:  removed  commands  as it was replaced by function call

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { onAnyMessage } from './whatsapp/index.js';
-import * as commands from './commands/index.js';
 import { createBot } from  './whatsapp/index.js'
 
-const start = async client => client.onAnyMessage( message => onAnyMessage(client, message, commands) );
+const start = async client => client.onAnyMessage( message => onAnyMessage(client, message) );
 createBot().then(start).catch(console.log)
