33af579c3eaf68c83e94e92c79f9fc36b19e4601
Bruno Costa Borges <brunocosta@wolfplace.solutions>
2025-02-04 00:31:13 -0300

    fix:  removed  commands  as it was replaced by function call

--- src/whatsapp/onAnyMessage.js
+++ src/whatsapp/onAnyMessage.js
@@ -1,15 +1,13 @@
 import { getResponseHandler } from "./responseHandlers.js";
+import {getTextResponse} from '../open-ai/functions/getTextResponse.js'
 
-export const onAnyMessage = (client, message, commands) => {
-
-    let clientCommand = message.text.substring(0, message.text.indexOf(" "));
-    let clientRequest = message.text.substring(message.text.indexOf(" "));
-
-    var command = commands.getCommand(clientCommand)
-    if(command) execCommand(command)
-
-    function execCommand (command) {
-        const responseHandler  = getResponseHandler(command.responseType)
-        command.action(clientRequest).then(response => responseHandler(client, message, response, clientRequest))
+const repliedMessages = new Set()
+export const onAnyMessage = async (client, message) => {
+    if(message.from !== '5511959963068@c.us' && !repliedMessages.has(message.id)){
+        await client.startTyping(message.from);
+        repliedMessages.add(message.id)
+        const {responseType, fileName, data, text }  = await getTextResponse(message)
+        const responseHandler  = getResponseHandler(responseType)
+        await responseHandler(client, message, text ?? '', fileName, data)
     }
 }
