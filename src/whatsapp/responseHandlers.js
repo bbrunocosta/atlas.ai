03b6ae618f065521e1b6984a47c71fb45753766c
Bruno Costa Borges <brunocosta@wolfplace.solutions>
2025-02-04 00:25:39 -0300

    fix: response hadlers now returns text and images properly

--- src/whatsapp/responseHandlers.js
+++ src/whatsapp/responseHandlers.js
@@ -2,16 +2,16 @@ import * as responseTypes from "./responseTypes.js";
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-export const sendResponseAsText = (client, message, text ) => client.sendText(
+export const sendResponseAsText = async (client, message, text ) => await client.sendText(
     message.from === process.env.WHATSAPP_PHONE_NUMBER ? message.to : message.from,
     text
 )
 
-export const sendResponseAsImg= (client, message, imgUrl, imgDescription ) => client.sendImage(
+export const sendResponseAsImg = async (client, message, text, fileName, data ) => await client.sendImageFromBase64(
     message.from === process.env.WHATSAPP_PHONE_NUMBER ? message.to : message.from,
-    imgUrl,
-    imgDescription,
-    'Imagem gerada pela IA DALL-E 🤖'
+    data,
+    fileName,
+    text
 );
 
 const responseHandlerCollection = {
