83b60fa4c0e1873eabd568516448645a7f8e49cd
Bruno Costa Borges <brunocosta@wolfplace.solutions>
2025-02-04 00:28:24 -0300

    fix: venom now uses new Headless mode

--- src/whatsapp/venombot-configuration.js
+++ src/whatsapp/venombot-configuration.js
@@ -2,7 +2,8 @@ import { create } from 'venom-bot';
 
 const options = {
     session: 'chat-gpt',
-    multidevice: true
+    multidevice: true,
+    browserArgs: ['--headless=new', '--no-sandbox', '--disable-setuid-sandbox']
 }
 
 export const createBot = () => create(options)
