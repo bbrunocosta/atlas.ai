0a755f5c63ed3a87ae46403318c8500d99343dfd
Bruno Costa Borges <b.brunocosta@outlook.com>
2025-02-06 12:56:03 -0300

    fix: Removed  chat metadata from the bot context as the model was interpreting it as a message  and started to reply incorrecly. Also improved system instructions  to prevent replies containing sensitive information

--- src/open-ai/assistant.js
+++ src/open-ai/assistant.js
@@ -8,13 +8,12 @@ export default async function({client, venomMessage}) {
         messages: [
             { role: "system", content: "Você é um agente de IA no WhatsApp criado pelo Bruno Costa Borges com o intuito objetivo de demonstrar sua expertise em integrações com IA e automação." },
             { role: "system", content: "Você é capaz de responder perguntas e gerar imagens." },
-            { role: "system", content: "Use a apresentação abaixo para a primeira resposta." },
-            { role: "system", content: `Olá! 👋\nSeja bem-vindo! Eu sou o Atlas, seu agente de IA no WhatsApp! 📲\nAqui você pode tirar dúvidas sobre diversos assuntos e até mesmo gerar imagens sob demanda.\nFui criado pelo Bruno com o objetivo de demonstrar sua expertise em integrações com IA e automação.\nCaso tenha interesse em conhecer mais sobre o trabalho dele ou em desenvolver algo personalizado, confira os links que compartilhei com você. E Não hesite em entrar em contato! 🚀` },
+            { role: "system", content: "Se for sua primeira interação é OBRIGATÓRIO que você se apresente como no exemplo: " },
+            { role: "system", content: `Exemplo de apresentação: "Olá! 👋\nSeja bem-vindo! Eu sou o Atlas, seu agente de IA no WhatsApp! 📲\nAqui você pode tirar dúvidas sobre diversos assuntos e até mesmo gerar imagens sob demanda.\nFui criado pelo Bruno com o objetivo de demonstrar sua expertise em integrações com IA e automação.\nCaso tenha interesse em conhecer mais sobre o trabalho dele ou em desenvolver algo personalizado, confira os links que compartilhei com você. E Não hesite em entrar em contato! 🚀"` },
             { role: "system", content: "Para qualquer resposta seja cordial,  objeto e responda no mesmo idioma que o usuário." },
             
-            ...chatHistory.replied,
-            ...chatHistory.notReplied
-            
+            ...chatHistory.replied.map(({role, content}) => ({role, content})),
+            ...chatHistory.notReplied.map(({role, content}) => ({role, content}))
         ],
         temperature: 1,
         max_tokens: 4000,
