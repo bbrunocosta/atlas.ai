1c685bc706c665c39466dcf698278284bf949410
Bruno Costa Borges <b.brunocosta@outlook.com>
2025-02-06 12:45:23 -0300

    BREAKING CHANGE: Migrates from Venom to WPPConnect enabling user typing state tracking and   debouncing of interactions. This change may require adjustments in method calls or event listeners related to message tracking.

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,35 @@
-import { createVenomBot } from './whatsapp/venombot-configuration.js';
+import { createWppconnectBot } from './whatsapp/wppconnect-configuration.js';
 import { saveMessage } from './data/sqlite-repository.js';
 import assistant from './open-ai/assistant.js';
-const client = await createVenomBot()
+const client = await createWppconnectBot()
+
+const pendingMessages = {
+  messages: new Map(),
+  clocks: new Map(),
+  currentState: new Map()
+}
+
 
 try
 {
+  client.onPresenceChanged((args) => {
+    if(!args) return;
+    
+    console.log(args.state, pendingMessages.messages.has(args.id), pendingMessages.clocks.has(args.id))
+
+    if(args.state === 'unavailable' && pendingMessages.messages.has(args.id)){
+      waitSecondsThenRunAgent(5, args.id)
+    }
+    
+    else if(pendingMessages.clocks.has(args.id)) {
+      clearTimeout(pendingMessages.clocks.get(args.id))
+      pendingMessages.clocks.delete(args.id)
+
+    }
+  });
+
+
   client.onAnyMessage( async message => {   
-
-    console.log( message.fromMe, 'onAnyMessage', message.id, message.body)
-
     const messageWasSaved = await saveMessage({ 
       messageId: message.id, 
       chatId: message.fromMe ? message.to : message.from, 
@@ -16,8 +37,12 @@ try
       role: message.fromMe ? 'assistant' : 'user',
       isReplied: message.fromMe ? 1 : 0
     })
+    console.log( 'onAnyMessage', !message.fromMe,  messageWasSaved)
 
-    if(messageWasSaved && !message.fromMe) await assistant({client, venomMessage: message})
+    if(messageWasSaved && !message.fromMe) {
+      pendingMessages.messages.set(message.from, message)
+      waitSecondsThenRunAgent(5, message.from)
+    }
   })
 }
 
@@ -26,4 +51,18 @@ catch(error)
 {
   console.log(error)
   client.close();
+}
+
+function waitSecondsThenRunAgent(seconds, chatId){
+  if(pendingMessages.clocks.has(chatId)) {
+    clearTimeout(pendingMessages.clocks.get(chatId))
+    pendingMessages.clocks.delete(chatId);
+  }
+
+  pendingMessages.clocks.set(chatId, setTimeout(async () => {
+    const message = pendingMessages.messages.get(chatId)
+    pendingMessages.messages.delete(chatId)
+    pendingMessages.clocks.delete(chatId); 
+    await assistant({client, venomMessage: message})
+  }, seconds * 1000))
 }
