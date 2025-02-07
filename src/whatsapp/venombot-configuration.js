94c032d758c10202fc6c5b097c4d128a8580cfc0
Bruno Costa Borges <b.brunocosta@outlook.com>
2025-02-07 01:20:59 -0300

    refactor: removes venom bot as it was replaced with  wppconnect

--- src/whatsapp/venombot-configuration.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'venom-bot';
-
-export const options = {
-    session: 'chat-gpt',
-    multidevice: false,
-    browserArgs: ['--headless=new', '--no-sandbox', '--disable-setuid-sandbox']
-}
-
-export const createVenomBot = () => create(options)
